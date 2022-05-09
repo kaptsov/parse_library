a2597c5a5a83e03a02ece15d32041f1260a11352
kaptsov <kaptsovaa@gmail.com>
2022-05-09 20:39:17 +0400

    Добавлен check for redirect

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import requests
 import os
 
+from requests import HTTPError
+
 DIR = 'books'
 
-try:
-    os.makedirs(DIR)
-except FileExistsError:
-    # directory already exists
-    pass
+
+def make_dirs():
+    try:
+        os.makedirs(DIR)
+    except FileExistsError:
+        # directory already exists
+        pass
 
 
-for book in range(10):
+def check_for_redirect(history):
+    try:
+        return history
+    except HTTPError:
+        pass
 
-    url = f'https://tululu.org/txt.php?id=321{book}'
 
-    response = requests.get(url)
-    response.raise_for_status()
+def main():
 
-    filename = f'{DIR}/text{book}.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    for book in range(10):
+
+        url = f'https://tululu.org/txt.php?id=321{book}'
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        if check_for_redirect(response.history):
+            filename = f'{DIR}/text{book}.txt'
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+
+
+if __name__ == "__main__":
+    main()
