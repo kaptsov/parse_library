e283bae5309eb49485c34c3a7db3d76b4b9e750b
kaptsov <kaptsovaa@gmail.com>
2022-05-09 21:16:21 +0400

    Файлы именуются полным названием

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import requests
 import os
+from bs4 import BeautifulSoup
 
+import requests
 from requests import HTTPError
 
 DIR = 'books'
@@ -21,18 +22,28 @@ def check_for_redirect(history):
         pass
 
 
+def get_book_name(url):
+    page_content = requests.get(url).text
+    soup = BeautifulSoup(page_content, 'lxml').find(id='content')
+    book_title, book_author = soup.find('h1').text.split('::')
+    return book_title.strip(), book_author.strip()
+
+
 def main():
 
     make_dirs()
+
     for book in range(10):
 
-        url = f'https://tululu.org/txt.php?id=321{book}'
+        url = f'https://tululu.org/txt.php?id={book}'
+        page_url = f'https://tululu.org/b{book}/'
 
         response = requests.get(url)
         response.raise_for_status()
 
         if not check_for_redirect(response.history):
-            filename = f'{DIR}/text{book}.txt'
+            book_title, book_author = get_book_name(page_url)
+            filename = f'{DIR}/{book_author} - {book_title}.txt'
             with open(filename, 'wb') as file:
                 file.write(response.content)
 
