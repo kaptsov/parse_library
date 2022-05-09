8dd76524b4777fef369ad5ceb9371d5005b093b1
kaptsov <kaptsovaa@gmail.com>
2022-05-09 21:30:58 +0400

    Выводятся ссылки на обложки (не все)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from urllib.parse import urljoin
+
 from bs4 import BeautifulSoup
 
 import requests
@@ -22,9 +24,14 @@ def check_for_redirect(history):
         pass
 
 
-def get_book_name(url):
-    page_content = requests.get(url).text
-    soup = BeautifulSoup(page_content, 'lxml').find(id='content')
+def get_book_image(url, page_content):
+    soup = BeautifulSoup(page_content.text, 'lxml').find(id='content')
+    image_url = urljoin(url, soup.find('div', class_='bookimage').find('img')['src'])
+    return  image_url
+
+
+def get_book_name(page_content):
+    soup = BeautifulSoup(page_content.text, 'lxml').find(id='content')
     book_title, book_author = soup.find('h1').text.split('::')
     return book_title.strip(), book_author.strip()
 
@@ -38,14 +45,17 @@ def main():
         url = f'https://tululu.org/txt.php?id={book}'
         page_url = f'https://tululu.org/b{book}/'
 
-        response = requests.get(url)
-        response.raise_for_status()
+        book_text = requests.get(url)
+        book_text.raise_for_status()
+        page_response = requests.get(page_url)
+        page_response.raise_for_status()
 
-        if not check_for_redirect(response.history):
-            book_title, book_author = get_book_name(page_url)
+        if not check_for_redirect(book_text.history):
+            book_title, book_author = get_book_name(page_response)
+            print(get_book_image(page_url, page_response))
             filename = f'{DIR}/{book_author} - {book_title}.txt'
             with open(filename, 'wb') as file:
-                file.write(response.content)
+                file.write(book_text.content)
 
 
 if __name__ == "__main__":
