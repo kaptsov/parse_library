9afece3e784a857e596ec710c45fe433c21863b3
alexanderkaptsov <kaptsovaa@gmail.com>
2022-06-08 23:28:31 +0400

    Step 3

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -5,13 +5,16 @@ from bs4 import BeautifulSoup
 
 import main
 
-url = 'https://tululu.org/l55/'
-response = requests.get(url, timeout=5)
-main.raise_for_redirect(response.history)
-bookpage_content = response.content
-soup = BeautifulSoup(bookpage_content, 'lxml').find(id='content')
-book_table = soup.find_all('table')
-for book in book_table:
-    print(urljoin('https://tululu.org/', book.find('a').attrs['href']))
+
+for page in range(1, 5):
+
+    url = f'https://tululu.org/l55/{page}/'
+    response = requests.get(url, timeout=5)
+    main.raise_for_redirect(response.history)
+    bookpage_content = response.content
+    soup = BeautifulSoup(bookpage_content, 'lxml').find(id='content')
+    book_table = soup.find_all('table')
+    for book in book_table:
+        print(urljoin('https://tululu.org/', book.find('a').attrs['href']))
 
 
